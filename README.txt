8bb4c5e6fdd6283e81df399bb21665e61bb86f45
JVJohnson <james.vinton.johnson@gmail.com>
2017-10-31 20:45:51 -0400

    first commit- accuracy/precision test

---
+++ README.txt
@@ -0,0 +1,41 @@
+README.txt for the Uncertainty test python file
+Author: James Johnson
+
+The UncertaintyTestForAccuracy.py file manually calculates the result and error of three calculations using regular python math functions
+The file then compares it to the automatic error propagation done by the Uncertainty Python package. 
+This file exists to prove that Uncertainty provides accurate and precise enough results to trust in Dr. Whitbeck's Physics C Lab.
+
+
+
+---------------------------------------------------------
+RUN INSTRUCTIONS
+
+1. -have python 3 installed (3.5.2)
+2. -have uncertainty installed for said version
+3.a-run this command in a linux terminal"python3 UncertaintyTestForAccuracy.py"
+	.b) Otherwise execute the python file however is required for your system. 
+4. Program should run and display the output
+
+
+---------------------------------------------------------
+OUTPUT
+
+
+Multiplication Check!:
+Result:      18.000000000000000+/-0.900222194794152
+Expected:    18.000000000000000+/-0.900222194794152+/-0.900222194794152
+Calculation:   Correct!
+Error:         Correct!
+
+Addition Check!:
+Result:      14.000000000000000+/-0.050990195135928
+Expected:     14.0+/-0.050990195135927854
+Calculation:   Correct!
+Error:         Correct!
+
+Function Check!:
+0.5235987755982988
+Result:      0.86602540378443860+/-0.00872664625997165
+Expected:    0.8660254037844386+/-0.0087266462599716495
+Calculation:   Correct!
+Error:         Correct!
---
+++ UncertaintyTestForAccuracy.py
@@ -0,0 +1,113 @@
+from uncertainties import *
+from uncertainties.umath import *
+
+EPSILON = 0.0000000001  #because uncertainty appearantly has more significant digits in error than regular python's floats? wow. 
+
+
+
+#Checks Multiplication
+print("\nMultiplication Check!:")
+
+#definition
+a       =   9
+aErr    = .01
+b       =   2
+bErr    = 0.1
+
+#conversion to uncertainty type
+aFl     = ufloat(a, aErr)
+bFl     = ufloat(b, bErr)
+
+#calculation
+uncResult = aFl*bFl
+
+result = a*b
+desErr = result*((aErr/a)**2 + (bErr/b)**2)**.5
+
+#print statements so humans can make sure its right
+print("Result:      {:5.17}".format(uncResult))
+print("Expected:    {:5.17}+/-{:5.17}".format(uncResult, desErr))
+
+#computerized results for quick veiwing
+if(abs(uncResult.n - float(result)) < EPSILON):
+    print("Calculation:   Correct!")
+else:
+    print("Calculation: Incorrect!")
+
+
+if(abs(uncResult.s - float(desErr)) < EPSILON):
+    print("Error:         Correct!")
+else:
+    print("Error:       Incorrect!")
+
+
+
+#Checks Addition
+print("\nAddition Check!:")
+
+#definition
+a       =   5
+aErr    = .01
+b       =   9
+bErr    = .05
+
+#conversion to uncertainty type
+aFl     = ufloat(a, aErr)
+bFl     = ufloat(b, bErr)
+
+#calculation
+uncResult = aFl+bFl
+result = a + b
+desErr = ((aErr)**2 + (bErr)**2)**.5
+
+#print statements so humans can make sure its right
+print("Result:      {:5.17}".format(uncResult))
+print("Expected:    {:5.17}+/-{:5.17}".format(float(result), desErr))
+
+#computerized results for quick veiwing
+if(abs(uncResult.n - float(result)) < EPSILON):
+    print("Calculation:   Correct!")
+else:
+    print("Calculation: Incorrect!")
+
+
+if(abs(uncResult.s - float(desErr)) < EPSILON):
+    print("Error:         Correct!")
+else:
+    print("Error:       Incorrect!")
+
+
+
+#Checks functions with sine
+print("\nFunction Check!:")
+
+#definition
+a       =   radians(30)
+aErr    =   radians(.5)
+print(a)
+
+#conversion to uncertainty type
+aFl     = ufloat(a, aErr)
+
+
+#calculation
+uncResult = sin(2 * aFl)
+result = sin(2*a)
+desErr = 2 * cos(2 * a) *aErr
+
+#print statements so humans can make sure its right
+print("Result:      {:5.17}".format(uncResult))
+print("Expected:    {:5.17}+/-{:5.17}".format(float(result), float(desErr)))
+
+#computerized results for quick veiwing
+if(abs(uncResult.n - float(result)) < EPSILON):
+    print("Calculation:   Correct!")
+else:
+    print("Calculation: Incorrect!")
+
+
+if(abs(uncResult.s - float(desErr)) < EPSILON):
+    print("Error:         Correct!")
+else:
+    print("Error:       Incorrect!")
+
